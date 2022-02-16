2e2107e9681c678016b10bb56ea2cf0416c653f9
AlexeiCocu <alexei.cocu@gmail.com>
2022-02-16 12:49:36 +0200

    d

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 const FeedbackList = () => {
-    const { feedback } = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0){
+    if(!isLoading && (!feedback || feedback.length === 0)){
         return 'No Feedback Yet'
     }
 
-    return (
+
+    return isLoading ? <Spinner /> : (
+
         <div className='feedback-list'>
             <AnimatePresence>
                 {feedback.map((item) => (
@@ -22,6 +25,8 @@ const FeedbackList = () => {
             </AnimatePresence>
         </div>
     )
+
+
 
     // return (
     //     <div className='feedback-list'>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,36 +1,44 @@
-import {createContext, useState} from 'react';
-import {v4 as uuidv4} from "uuid";
+import {createContext, useState, useEffect} from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
 
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is feedback 1',
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This is feedback 2',
-            rating: 3
-        },
-        {
-            id: 3,
-            text: 'This is feedback 3',
-            rating: 7
-        }
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
-    const addFeedback = ( newFeedback ) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    useEffect(()=>{
+        fetchFeedback()
+    }, [])
+
+    //fetch data from db.json
+    const fetchFeedback = async () => {
+        const response = await fetch('/feedback?_sort=id&_order=desc');
+        const data = await response.json();
+        setFeedback(data);
+        setIsLoading(false)
+    }
+
+    // add feedback
+    const addFeedback = async ( newFeedback ) => {
+
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback])
     }
 
     // set item to be updated
@@ -42,12 +50,28 @@ export const FeedbackProvider = ({ children }) => {
     }
 
     // Update item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item ))
+    const updateFeedback = async (id, updItem) => {
+
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json();
+
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item ))
     }
 
-    const deleteFeedback = (id) => {
+
+    // delete item
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete?')){
+
+            await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -56,6 +80,7 @@ export const FeedbackProvider = ({ children }) => {
         value={{
             feedback,
             feedbackEdit,
+            isLoading,
             deleteFeedback,
             addFeedback,
             editFeedback,
